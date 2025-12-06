3ea495852f5fa6b8cbc89c696aea250016b97f7d
Melwin Mathew <mathewmelwin20073@gmail.com>
2025-12-06 22:32:23 +0530

    Create swap.c

---
+++ Lab programs/swap.c
@@ -0,0 +1,14 @@
+//Program to swap two numbers
+#include <stdio.h>
+{
+int a, b, temp;
+printf("Enter the two numbers:\n");
+scanf("%d%d",&a,&b);
+printf("Before swapping: %d\t%d\n",a,b);
+temp=a;
+a=b;
+b=temp;
+printf("After swapping: %d\t%d\n",a,b);
+}
+
+  
